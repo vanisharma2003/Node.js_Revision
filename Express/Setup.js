13b72cca76daa1aca1673f332a65f48b016611ec
st-vani <137780818+vanisharma2003@users.noreply.github.com>
2025-08-05 22:25:08 +0530

    first get and post request

--- Express/Setup.js
+++ Express/Setup.js
@@ -4,3 +4,10 @@ let port = 3000
 app.listen(port,()=>{
     console.log(`App is listening on ${port}`)
 })
+app.use(express.json())
+app.get("/",(req,res)=>{
+    res.send("you contact root path")
+})
+app.post("/first-post-request",(req,res)=>{
+    res.json(req.body)
+})
